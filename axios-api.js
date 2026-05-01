43c2f01baa61511f1db50403c2cce705f32e4a4d
IrinaV1 <122563375+IrinaV1@users.noreply.github.com>
2026-05-01 15:28:15 +0000

    Deploying to gh-pages from @ IrinaV1/js-practice-l11@6acf167da5cd6afdd6c05ffa3ffd541687c00d38 🚀

--- axios-api.js
+++ axios-api.js
@@ -1,5 +1,10 @@
-import"./assets/styles-DSbSV4YY.js";import{a as c}from"./assets/vendor-Do60_h77.js";const r=document.querySelector(".todo-list");c("https://jsonplaceholder.typicode.com/todos",{params:{_limit:5}}).then(e=>{console.log(e.data),r.insertAdjacentHTML("beforeend",a(e.data))}).catch(e=>{console.log(e.message)});function a(e){return e.map(({id:t,title:o,completed:n})=>`<li data-id ="${t}" class="list-item">
-    <input type="checkbox" ${n&&"checked"}/>
-    <p>${o}</p>
-  </li>`).join("")}document.querySelector("#search-form");const i=document.querySelector("#breeds-list");document.querySelector("#breed-input");document.querySelector("#loader");document.querySelector("#cat-card");s();function s(){c("https://api.thecatapi.com/v1/breeds").then(({data:e})=>{console.log(e),i.insertAdjacentHTML("beforeend",d(e))}).catch(e=>{console.log(e.message)})}function d(e){return e.map(({id:t,name:o})=>`<option value="${o}" data-id="${t}"></option>`).join("")}
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{a as r}from"./assets/vendor-Do60_h77.js";const l=document.querySelector(".todo-list");r("https://jsonplaceholder.typicode.com/todos",{params:{_limit:5}}).then(e=>{console.log(e.data),l.insertAdjacentHTML("beforeend",u(e.data))}).catch(e=>{console.log(e.message)});function u(e){return e.map(({id:t,title:c,completed:s})=>`<li data-id ="${t}" class="list-item">
+      <input type="checkbox" ${s&&"checked"}/>
+    <p>${c}</p>
+  </li>`).join("")}const m=document.querySelector("#search-form"),i=document.querySelector("#breeds-list"),p=document.querySelector("#breed-input"),n=document.querySelector("#loader"),a=document.querySelector("#cat-card");m.addEventListener("submit",g);h();function h(){r("https://api.thecatapi.com/v1/breeds").then(({data:e})=>{console.log(e),i.insertAdjacentHTML("beforeend",f(e))}).catch(e=>{console.log(e.message)})}function f(e){return e.map(({id:t,name:c})=>`<option value="${c}" data-id="${t}"></option>`).join("")}function g(e){e.preventDefault(),a.innerHTML="";const t=p.value,c=[...i.options].find(o=>o.value.toLowerCase()===t.toLowerCase());if(!c){alert("Оберіть існуючу породу");return}const s=c.dataset.id;n.classList.remove("hidden"),r(`https://api.thecatapi.com/v1/images/search?breed_ids=${s}`).then(({data:o})=>{if(o.length>0){const d=o[0].url;a.innerHTML=`<div class="card">
+        <img src="${d}" alt="${t}" class="card-image"/>
+        <div class="card-body">
+        <h2 class="card-title">${t}</h2>
+        </div>
+        </div>`}else a.innerHTML='<p class="error-text">Зображення не знайдено</p>';console.log(o)}).catch(o=>{alert(o.message)}).finally(()=>{n.classList.add("hidden"),e.target.reset()})}
 //# sourceMappingURL=axios-api.js.map
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import"./assets/styles-DSbSV4YY.js";import{a as n,S as i,i as o}from"./assets/vendor-Do60_h77.js";const l="55630526-f44120b4f4ecc6b1cd7cfb12b",c="https://pixabay.com/api/";function p(t){return n.get(c,{params:{key:l,q:t.trim(),image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(a=>a.data.hits)}const r=document.querySelector(".gallery"),s=document.querySelector(".loader"),g=new i(".gallery a",{captionsData:"alt",captionDelay:250});function m(t){const a=t.map(e=>`
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{a as n,S as i,i as o}from"./assets/vendor-Do60_h77.js";const l="55630526-f44120b4f4ecc6b1cd7cfb12b",c="https://pixabay.com/api/";function p(t){return n.get(c,{params:{key:l,q:t.trim(),image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(a=>a.data.hits)}const r=document.querySelector(".gallery"),s=document.querySelector(".loader"),g=new i(".gallery a",{captionsData:"alt",captionDelay:250});function m(t){const a=t.map(e=>`
       <li class="gallery-item">
         <a href="${e.largeImageURL}">
             <img class="gallery-image" src="${e.webformatURL}" alt="${e.tags}" />
